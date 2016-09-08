3d7c31e55859f114f2fa1e880556a5b7544c4854
jumusk89 <jumusk89@yahoo.com>
2016-09-08 15:26:55 -0400

    Initial commit of project

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//Prevent spoilerphobes form seeing spoilers
+//Solution: Hide spoilers and reveal them through user interaction --- Who hasn't seen Star Wars?!!!
+
+//1. Hide spoiler
+$(".spoiler span").hide();
+//2, Add a button
+$(".spoiler").append("<button>Reveal Spoiler!!</button>");
+//3, When button pressed
+$("button").click(function(){
+	//3.1, Show spoiler
+	$(this).prev().show();
+	//3.2 Get rid of button
+	$(this).remove();
+});
+
+	
+	
